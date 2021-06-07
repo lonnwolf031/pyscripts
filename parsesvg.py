86d9dcda31ea2147ce83b55e60a5af7442dab9d3
Lonn <lonn.wolf031@gmail.com>
2021-06-07 14:03:08 +0200

    update

--- parsesvg.py
+++ parsesvg.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 soup = BeautifulSoup('<svg></svg>', 'xml')
 
 
-with open("kaarten_regrouped.svg", "r") as file:
+with open("test.svg", "r") as file:
     # Read each line in the file, readlines() returns a list of lines
     content = file.readlines()
     # Combine the lines in the list into a string
@@ -23,12 +23,17 @@ with open("kaarten_regrouped.svg", "r") as file:
         card = cardname + str(i)
         new_svg = soup.new_tag("svg")
         new_svg['xmlns'] = "http://www.w3.org/2000/svg"
-        thing.insert_before(new_svg)
+
+       # foo = thing.insert_before(new_svg)
+        new_svg.append(thing)
+        # thing.insert_before(new_svg)    #might replace above
         i += 1
-        with open('%s.svg' % card, 'w') as file:
-            file.write(str(thing))
-            pass
-       # print(thing)
+        print(new_svg)
+        print("\n\n\n")
+       # with open('%s.svg' % card, 'w') as file:
+           # file.write(str(thing))
+         #  pass
+
     # make this svg parent tag
 
     # add attribute   xmlns="http://www.w3.org/2000/svg"
--- souptest.py
+++ souptest.py
@@ -9,10 +9,10 @@ with open("test.svg", "r") as file:
     content = file.readlines()
     # Combine the lines in the list into a string
     content = "".join(content)
-    #bs_content = bs(content, "lxml")
+    # bs_content = bs(content, "lxml")
     soup = BeautifulSoup(content, 'xml')
     svgfile = soup.find('svg')
-    #groups = svgfile.findChildren('g')
+    # groups = svgfile.findChildren('g')
     singlecard = svgfile.findChildren('g')
 
     cardname = "test"
@@ -22,18 +22,6 @@ with open("test.svg", "r") as file:
         card = cardname + str(i)
         new_svg = soup.new_tag("svg")
         new_svg['xmlns'] = "http://www.w3.org/2000/svg"
-        thing.insert_before(new_svg)
+        thing = thing.insert_before(new_svg)
         i += 1
-        #with open('%s.svg' % card, 'w') as file:
-        #    file.write(str(thing))
-        #    pass
-       print(thing)
-    # make this svg parent tag
-
-    # add attribute   xmlns="http://www.w3.org/2000/svg"
-    # each thing.insert_before(new_div)
-    #print(groups)
-
-
-
-#print(soup)
+        print(thing)
